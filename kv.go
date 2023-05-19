68512a61218d46b288eccc044afcfd73990df69b
whl <41020733@qq.com>
2023-05-19 20:12:16 +0800

    feat: add MergeKV函数

--- kv.go
+++ kv.go
@@ -5,3 +5,11 @@ import "go.mongodb.org/mongo-driver/bson"
 func KV(key string, value interface{}) bson.D {
 	return bson.D{{Key: key, Value: value}}
 }
+
+func MergeKV(values ...bson.D) bson.D {
+	var fields bson.D
+	for _, value := range values {
+		fields = append(fields, value...)
+	}
+	return fields
+}
